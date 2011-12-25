3ed6fffecd0239a2cd95cb51a412cbaed194959f
Leon P Smith <leon@melding-monads.com>
2011-12-25 02:40:20 -0500

    Rudimentary Large Object support

---
+++ src/Database/PostgreSQL/Simple/LargeObjects.hs
@@ -0,0 +1,26 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Database.PostgreSQL.Simple.LargeObjects
+-- Copyright   :  (c) 2011 Leon P Smith
+-- License     :  BSD3
+--
+-- Maintainer  :  leon@melding-monads.com
+--
+-----------------------------------------------------------------------------
+
+module Database.PostgreSQL.Simple.LargeObjects
+     ( loImport
+     , loExport
+     , Oid(..)
+     ) where
+
+import           Database.PostgreSQL.LibPQ (Oid(..))
+import qualified Database.PostgreSQL.LibPQ as PQ
+import           Database.PostgreSQL.Simple.Implementation
+import           Foreign.C.Types(CInt)
+
+loImport :: Connection -> FilePath -> IO (Maybe Oid)
+loImport conn path = withConnection conn $ \c -> PQ.loImport c path 
+
+loExport :: Connection -> Oid -> FilePath -> IO CInt
+loExport conn oid path = withConnection conn $ \c -> PQ.loExport c oid path
