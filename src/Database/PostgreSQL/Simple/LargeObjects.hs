23ab08d2f30369d8a98dd9b28597462b9f30b6ee
Leon P Smith <leon@melding-monads.com>
2011-12-30 00:47:05 -0500

    Large Object tweaks

--- src/Database/PostgreSQL/Simple/LargeObjects.hs
+++ src/Database/PostgreSQL/Simple/LargeObjects.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Database.PostgreSQL.Simple.LargeObjects
@@ -14,13 +16,32 @@ module Database.PostgreSQL.Simple.LargeObjects
      , Oid(..)
      ) where
 
+import           Control.Applicative ((<$>))
+import           Control.Exception (throwIO)
 import           Database.PostgreSQL.LibPQ (Oid(..))
 import qualified Database.PostgreSQL.LibPQ as PQ
 import           Database.PostgreSQL.Simple.Internal
 import           Foreign.C.Types(CInt)
 
-loImport :: Connection -> FilePath -> IO (Maybe Oid)
-loImport conn path = withConnection conn $ \c -> PQ.loImport c path
+loImport :: Connection -> FilePath -> IO Oid
+loImport conn path = withConnection conn $ \c -> do
+    res <- PQ.loImport c path
+    case res of
+      Nothing -> do
+          msg <- maybe "loImport error" id <$> PQ.errorMessage c
+          throwIO $ SqlError { sqlNativeError = -1   -- FIXME?
+                             , sqlErrorMsg    = msg
+                             , sqlState       = ""  }
+      Just  x -> return x
 
-loExport :: Connection -> Oid -> FilePath -> IO (Maybe ())
-loExport conn oid path = withConnection conn $ \c -> PQ.loExport c oid path
+
+loExport :: Connection -> Oid -> FilePath -> IO ()
+loExport conn oid path = withConnection conn $ \c -> do
+    res <- PQ.loExport c oid path
+    case res of
+      Nothing -> do
+          msg <- maybe "loExport error" id <$> PQ.errorMessage c
+          throwIO $ SqlError { sqlNativeError = -1   -- FIXME?
+                             , sqlErrorMsg    = msg
+                             , sqlState       = ""  }
+      Just  x -> return x
