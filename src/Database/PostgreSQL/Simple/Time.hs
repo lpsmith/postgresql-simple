7c7c02d6baf5aea8ebcf64b06d9708dc2b3d377f
Leon P Smith <leon@melding-monads.com>
2012-06-02 23:52:34 -0400

    Comments

--- src/Database/PostgreSQL/Simple/Time.hs
+++ src/Database/PostgreSQL/Simple/Time.hs
@@ -6,9 +6,26 @@
 -- Maintainer:  Leon P Smith <leon@melding-monads.com>
 -- Stability:   experimental
 --
--- Time types that supports infinities.   Also includes new time parsers
--- with improved performance over GHC's time package.  See
--- 'Database.PostgreSQL.Simple.Time.Internal'.
+-- Time types that supports positive and negative infinity.   Also includes 
+-- new time parsers and printers with better performance than GHC's time package.
+-- The parsers only understand the specific variant of ISO 8601 that PostgreSQL 
+-- emits,  and the printers attempt to duplicate this syntax.  These likely have
+-- problems and shortcomings.  Some that I know of:
+--
+-- 1. Timestamps with time zones before @1883-Nov-18 12:00:00-05@, the moment
+--    Standard Railway Time went live,  cannot be parsed.  This is because
+--    PostgreSQL will emit @1883-11-18 12:03:57-04:56:02@ instead of
+--    @1883-11-18 11:59:59-05@,  and the timezone parser is incomplete.
+--    Timestamps without time zones do not have this problem.
+--
+-- 2. Dates an times surrounding @1582-Feb-24@,  the date the Gregorian 
+--    Calendar was introduced,  should be investigated for conversion errors.
+--
+-- 3. Points in time Before Christ are not also not supported.  For example,
+--    PostgreSQL will emit @0045-01-01 BC@ for a value of a @date@ type.
+--    This is the year that the Julian Calendar was adopted.
+--
+-- However, it should be noted that the old parsers also had these issues.
 --
 ------------------------------------------------------------------------------
 
--- src/Database/PostgreSQL/Simple/Time/Internal.hs
+++ src/Database/PostgreSQL/Simple/Time/Internal.hs
@@ -6,15 +6,6 @@
 -- Maintainer:  Leon P Smith <leon@melding-monads.com>
 -- Stability:   experimental
 --
--- Time parsers with improved performance over GHC's time package.  They
--- only understand the specific variant of ISO 8601 that PostgreSQL emits.
--- These parsers likely have problems and shortcomings.  Some that I know of:
---
--- 1. Timestamps with time zones before 1883-Nov-18 are not supported, due to
---    incomplete time zone parsing.
---
--- 2. Time Before Christ is not also not supported.
---
 ------------------------------------------------------------------------------
 
 {-# LANGUAGE DeriveDataTypeable  #-}
