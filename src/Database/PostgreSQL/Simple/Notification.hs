4f6066d3e10f31cabac42545a4802455757915d9
Leon P Smith <leon@melding-monads.com>
2015-12-26 05:34:48 -0500

    Have getNotification use threadWaitReadSTM where available

--- src/Database/PostgreSQL/Simple/Notification.hs
+++ src/Database/PostgreSQL/Simple/Notification.hs
@@ -5,7 +5,8 @@
 -----------------------------------------------------------------------------
 -- |
 -- Module      :  Database.PostgreSQL.Simple.Notification
--- Copyright   :  (c) 2011-2012 Leon P Smith
+-- Copyright   :  (c) 2011-2015 Leon P Smith
+--                (c) 2012 Joey Adams
 -- License     :  BSD3
 --
 -- Maintainer  :  leon@melding-monads.com
@@ -35,13 +36,23 @@ module Database.PostgreSQL.Simple.Notification
      , getBackendPID
      ) where
 
-import           Control.Concurrent
-import           Control.Monad ( when )
-import           Control.Exception ( throwIO )
+import           Control.Monad ( join, void )
+import           Control.Exception ( throwIO, try )
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as B8
 import           Database.PostgreSQL.Simple.Internal
 import qualified Database.PostgreSQL.LibPQ as PQ
 import           System.Posix.Types ( CPid )
+import           GHC.IO.Exception ( ioe_location )
+
+#if defined(mingw32_HOST_OS)
+import           Control.Concurrent ( threadDelay )
+#elif !MIN_VERSION_base(4,7,0)
+import           Control.Concurrent ( threadWaitRead )
+#else
+import           GHC.Conc           ( atomically )
+import           Control.Concurrent ( threadWaitReadSTM )
+#endif
 
 data Notification = Notification
    { notificationPid     :: !CPid
@@ -59,33 +70,69 @@ convertNotice PQ.Notify{..}
 --   'getNotification' blocks until one arrives.
 
 getNotification :: Connection -> IO Notification
-getNotification conn = loop False
+getNotification conn = join $ withConnection conn fetch
   where
     funcName = "Database.PostgreSQL.Simple.Notification.getNotification"
-    loop doConsume = do
-        res <- withConnection conn $ \c -> do
-                         when doConsume (PQ.consumeInput c >> return ())
-                         mmsg <- PQ.notifies c
-                         case mmsg of
-                           Nothing -> do
-                                         mfd <- PQ.socket c
-                                         case mfd of
-                                           Nothing -> throwIO $ fdError funcName
-                                           Just fd -> return (Left fd)
-                           Just msg -> return (Right msg)
-        -- FIXME? what happens if the connection is closed/reset right here?
-        case res of
+
+    fetch c = do
+        mmsg <- PQ.notifies c
+        case mmsg of
+          Just msg -> return (return $! convertNotice msg)
+          Nothing -> do
+              mfd <- PQ.socket c
+              case mfd of
+                Nothing  -> return (throwIO $! fdError funcName)
 #if defined(mingw32_HOST_OS)
-          -- threadWaitRead doesn't work for sockets on Windows, so just poll
-          -- for input every second (PQconsumeInput is non-blocking).
-          --
-          -- We could call select(), but FFI calls can't be interrupted with
-          -- async exceptions, whereas threadDelay can.
-          Left _fd -> threadDelay 1000000 >> loop True
+                -- threadWaitRead doesn't work for sockets on Windows, so just
+                -- poll for input every second (PQconsumeInput is non-blocking).
+                --
+                -- We could call select(), but FFI calls can't be interrupted
+                -- with async exceptions, whereas threadDelay can.
+                Just _fd -> do
+                  return (threadDelay 1000000 >> loop)
+#elif !MIN_VERSION_base(4,7,0)
+                -- Technically there's a race condition that is usually benign.
+                -- If the connection is closed or reset after we drop the
+                -- lock,  and then the fd index is reallocated to a new
+                -- descriptor before we call threadWaitRead,  then
+                -- we could end up waiting on the wrong descriptor.
+                --
+                -- Now, if the descriptor becomes readable promptly,  then
+                -- it's no big deal as we'll wake up and notice the change
+                -- on the next iteration of the loop.   But if are very
+                -- unlucky,  then we could end up waiting a long time.
+                Just fd  -> do
+                  return $ do
+                    merr <- try (threadWaitRead fd)
+                    case merr of
+                      Left  err -> throwIO $! setLoc err
+                      Right _   -> loop
 #else
-          Left fd -> threadWaitRead fd >> loop True
+                -- This case fixes the race condition above.   By registering
+                -- our interest in the descriptor before we drop the lock,
+                -- there is no opportunity for the descriptor index to be
+                -- reallocated on us.
+                --
+                -- (That is, assuming there isn't concurrently executing
+                -- code that manipulates the descriptor without holding
+                -- the lock... but such a major bug is likely to exhibit
+                -- itself in an at least somewhat more dramatic fashion.)
+                Just fd  -> do
+                  (waitRead, _) <- threadWaitReadSTM fd
+                  return $ do
+                    merr <- try (atomically waitRead)
+                    case merr of
+                      Left  err -> throwIO $! setLoc err
+                      Right _   -> loop
 #endif
-          Right msg -> return $! convertNotice msg
+
+    loop = join $ withConnection conn $ \c -> do
+             void $ PQ.consumeInput c
+             fetch c
+
+    setLoc :: IOError -> IOError
+    setLoc err = err { ioe_location = B8.unpack funcName }
+
 
 -- | Non-blocking variant of 'getNotification'.   Returns a single notification,
 -- if available.   If no notifications are available,  returns 'Nothing'.
