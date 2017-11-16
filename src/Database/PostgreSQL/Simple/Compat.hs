dc9d0f839ba2c9a50c15ad68638b1116abc31d87
Bardur Arantsson <bardur@scientician.net>
2017-11-16 08:33:41 +0100

    rm compat

--- src/Database/PostgreSQL/Simple/Compat.hs
+++ src/Database/PostgreSQL/Simple/Compat.hs
@@ -33,21 +33,14 @@ import System.IO.Unsafe (unsafeDupablePerformIO)
 import Data.Fixed (Pico)
 import Data.Fixed (Fixed(MkFixed))
 
--- | Like 'E.mask', but backported to base before version 4.3.0.
---
--- Note that the restore callback is monomorphic, unlike in 'E.mask'.  This
--- could be fixed by changing the type signature, but it would require us to
--- enable the RankNTypes extension (since 'E.mask' has a rank-3 type).  The
--- 'withTransactionMode' function calls the restore callback only once, so we
--- don't need that polymorphism.
+-- | Like 'E.mask', but with a monomorphic restore callback, unlike in
+-- 'E.mask'.  This could be fixed by changing the type signature, but
+-- it would require us to enable the RankNTypes extension (since
+-- 'E.mask' has a rank-3 type).  The 'withTransactionMode' function
+-- calls the restore callback only once, so we don't need that
+-- polymorphism.
 mask :: ((IO a -> IO a) -> IO b) -> IO b
-#if MIN_VERSION_base(4,3,0)
 mask io = E.mask $ \restore -> io restore
-#else
-mask io = do
-    b <- E.blocked
-    E.block $ io $ \m -> if b then m else E.unblock m
-#endif
 {-# INLINE mask #-}
 
 toByteString :: Builder -> ByteString
