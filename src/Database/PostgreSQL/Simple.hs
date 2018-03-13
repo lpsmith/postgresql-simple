eea54eb22133fcde8005b6325a39605864603796
Felix Paulusma <Vlix@users.noreply.github.com>
2018-03-14 00:33:56 +0100

    Escape double '??' to a literal '?'
    This change makes it possible to use literal question marks in queries to PostgreSQL. This is needed to be able to use the new JSON operators. https://www.postgresql.org/docs/9.5/static/functions-json.html
    
    This change can't break anything, since:
    
    * Anyone using double question marks would get an error if not supplied the adequate amount of accompanying values (in case of people trying the new JSON operators); or
    
    * If supplied the adequate amount of accompanying values, PostgreSQL would throw a fit because you'd supply two values directly next to each other, which doesn't make sense.

--- src/Database/PostgreSQL/Simple.hs
+++ src/Database/PostgreSQL/Simple.hs
@@ -281,7 +281,12 @@ buildQuery :: Connection -> Query -> ByteString -> [Action] -> IO Builder
 buildQuery conn q template xs =
     zipParams (split template) <$> mapM (buildAction conn q xs) xs
   where split s =
-            let (h,t) = B.break (=='?') s
+            let (h,t) = go (B.mempty,s)
+                go (x,bs) = (x `B.append` x',bs')
+                  where (h',t1) = B.break (=='?') bs
+                        (x',bs') = maybe (h',t1) go2 $ B.uncons t1 >>= B.uncons . snd
+                        go2 ('?',t2) = go (h' `snoc` '?',t2)
+                        go2 _ = (h',t1)
             in byteString h
                : if B.null t
                  then []
